dc9c0d541e8e8fba7e7d99dcc6e319a1381a2bb7
Dm2787 <damianmuttke@gmail.com>
2026-08-08 07:49:02 +0200

    Increment cache version to v4
    Updated cache version number to v4.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'phantom-cache-v3'; // ZMIEŃ TEN NUMER przy każdej aktualizacji (np. v2, v3)
+const CACHE_NAME = 'phantom-cache-v4'; // ZMIEŃ TEN NUMER przy każdej aktualizacji (np. v2, v3)
 const assetsToCache = [
   './index.html',
   './manifest.json',
