a7c41df6ac78a997aca63fd7d25d59885fd89c66
Dm2787 <damianmuttke@gmail.com>
2026-08-08 19:12:34 +0200

    Update cache version to phantom-cache-v13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'phantom-cache-v12'; // ZMIEŃ TEN NUMER przy każdej aktualizacji (np. v2, v3)
+const CACHE_NAME = 'phantom-cache-v13'; // ZMIEŃ TEN NUMER przy każdej aktualizacji (np. v2, v3)
 const assetsToCache = [
   './index.html',
   './manifest.json',
