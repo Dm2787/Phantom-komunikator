0c858734aeee48f63e9ed5b28a6bc65a6a410713
Dm2787 <damianmuttke@gmail.com>
2026-08-11 20:23:11 +0200

    Change cache version to v20
    Updated cache version number to v20.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'phantom-cache-v19'; // ZMIEŃ TEN NUMER przy każdej aktualizacji (np. v2, v3)
+const CACHE_NAME = 'phantom-cache-v20'; // ZMIEŃ TEN NUMER przy każdej aktualizacji (np. v2, v3)
 const assetsToCache = [
   './index.html',
   './manifest.json',
